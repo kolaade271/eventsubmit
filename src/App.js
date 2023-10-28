10e73fdd0a27534b2366fd5728f13f02f64888d4
cushybuy <adekola@paysnug.com>
2023-10-28 18:09:36 +0100

    new data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from "react";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import FormComponent from "./FormComponent";
 
 function App() {
   return (
     <div className="App">
-      <FormComponent />
-    </div>
+
+<BrowserRouter>
+       <Routes>
+       {/* <Route path="/" element={<SignUp/>} /> */}
+       <Route path="/:id" element={<FormComponent/>} />
+            </Routes>
+            </BrowserRouter>
+    </div> 
   );
 }
 
